d294b7227069c0a5349af37829132831e8f1b29f
Elisey <qqq@qqq.com>
2018-04-18 16:06:07 +0300

    eded

--- L3-oop/src/main/java/ru/track/list/ListMain.java
+++ L3-oop/src/main/java/ru/track/list/ListMain.java
@@ -6,6 +6,11 @@ package ru.track.list;
 public class ListMain {
 
     public static void main(String[] args) {
+        MyArrayList newList = new MyArrayList();
+        newList.add(5);
+        newList.add(4);
+        newList.remove(0);
+        newList.add(2);
 
     }
 }
